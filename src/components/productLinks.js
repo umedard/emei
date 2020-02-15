e5302af4edf73680c06df2b0a56d667929e63e4d
Medard <umedard@gmail.com>
2020-02-15 18:32:08 +0100

    change file name for linux

--- src/components/productLinks.js
+++ src/components/productLinks.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from "./ProductLinks.module.css"
+import style from "./productLinks.module.css"
 import {Link} from 'gatsby'
 
 export default function ProductLinks() {
