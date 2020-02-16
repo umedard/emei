d4db7d42a019a899f7450d9d6b2e292ed9aaa57f
Medard <umedard@gmail.com>
2020-02-16 15:05:02 +0100

    update templates

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -6,7 +6,6 @@ export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <p> Blog</p>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
