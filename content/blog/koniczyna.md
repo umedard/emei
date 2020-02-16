1f127c679d363fe4e2794bffb85ac28777df6c93
Medard <umedard@gmail.com>
2020-02-16 12:19:42 +0100

    home page revamp

--- content/blog/koniczyna.md
+++ content/blog/koniczyna.md
@@ -1,5 +1,5 @@
 ---
-language: en
+language: zh
 path: koniczynka
 date: 2020-02-02T18:12:15.279Z
 title: Koniczyna
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,50 @@
 import React from "react"
 import ProductList from "../components/productList"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    
-    <ProductList/>
-    <ProductList/>
 
-  </Layout>
-)
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .map(edge => <p>{edge.node.frontmatter.title}, {edge.node.frontmatter.language}</p>)
+  return (<Layout>
+  <SEO title="Home" />{Posts}
+  
+    <ProductList/>
+    <ProductList/>
+    
+    </Layout>)
+}
+
 
 export default IndexPage
+
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            language
+          }
+        }
+      }
+    }
+  }
+`
+
+
+
+
