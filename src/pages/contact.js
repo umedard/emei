616ec1e15e49eca7429700820510c43a1b90748b
Medard <umedard@gmail.com>
2020-02-24 13:27:24 +0100

    deconstruct variables

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,8 @@ import SEO from "../components/seo"
 import Highlighted from "../components/highlighted"
 import PageWrapper from "../components/pageWrapper"
 
+import style from './contact.module.css'
+
 
 const Contact = () => {
   const data = useStaticQuery(graphql`
@@ -17,13 +19,14 @@ const Contact = () => {
               fax
               phone
               location
+              email
             }
           }
         }
       }
     }
   `)
-  const contact = data.allMarkdownRemark.edges[0].node.frontmatter;
+  const {phone, fax, address, email} = data.allMarkdownRemark.edges[0].node.frontmatter;
   return (
     <Layout>
       <Highlighted text="Contact" imgUrl="/images/contact.jpeg" url="/contact" />
@@ -31,7 +34,33 @@ const Contact = () => {
 
        
           <PageWrapper>
-   Tel: {contact.phone} 
+   
+   <div className={style.contact__wrapper}>
+      <div className={style.contact__details}>
+           <p>Tel: {phone} </p> 
+           <p>Fax: {fax} </p> 
+           <p>Address: {address} </p> 
+           <p>Email {email} </p> 
+     
+      </div>
+
+      <form className={style.contact__form} name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <br></br><input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <br></br><input type="email" name="email" /></label>
+            </p>
+            
+            <p>
+              <label>Message: <br></br><textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+      </form>
+   </div>
+
 </PageWrapper>
 
     </Layout>
